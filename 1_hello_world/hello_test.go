3f62868355694a44b44e4ae8410f0384b89ea81b
Omar Flores <oflores@invidgroup.com>
2023-09-24 11:44:20 -0400

    Refactored test

--- 1_hello_world/hello_test.go
+++ 1_hello_world/hello_test.go
@@ -2,19 +2,22 @@ package main
 
 import "testing"
 
-//func TestHello(t *testing.T) {
-//	got := Hello()
-//	want := "Hello, world!1"
-//
-//	if got != want {
-//		t.Errorf("got %q want %q", got, want)
-//	}
-//}
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
 
-func TestHelloYou(t *testing.T) {
-	got := Hello("Omar")
-	want := "Hello, Omar"
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
