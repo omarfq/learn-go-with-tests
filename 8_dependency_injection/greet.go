dd060fd0bc22f2cac3b5d4b50893a7d01033dbbb
Omar Flores <oflores@invidgroup.com>
2023-10-28 11:16:20 -0400

    Add section on Dependency Injection

---
+++ 8_dependency_injection/greet.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+//type Writer interface {
+//	Write(p []byte) (n int, err error)
+//}
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreetHandler)))
+}
